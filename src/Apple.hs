e649241ae4220ebb05e39ba12bda69643c66adaa
Yotam Ohad <yotam2206@gmail.com>
2017-11-03 07:16:12 +0000

    moving to nixos

--- src/Apple.hs
+++ src/Apple.hs
@@ -1,37 +1,21 @@
+{-# LANGUAGE DataKinds #-}
 module Apple where
 
-import qualified Data.Vector as V
-import Numeric.LinearAlgebra
+import Data.Vector.Storable( (!)
+                  , Vector
+                  , fromList
+                  , toList )
+import Numeric.GSL.Minimization
 
-data Reversible a b = Reversible { forward :: a -> b
-                                 , backward :: b -> a}
+data Body = Body { pos :: (Vector Double) -> Double }
 
-revert :: Reversible a b -> Reversible b a
-revert r = Reversible (backward r) (forward r)
+circle :: Double -> Double -> Body
+circle x y = Body (\v -> (v!0-x)^2 + (v!1-y)^2 - 1)
 
-data Body a b = Body {unwrapList :: [(a -> b, a, a)]}
-instance Show a => Show (Body a b) where
-    show = show . map (\(f,mn, mx) -> (mn, mx)) . unwrapList
-
-class Boundry a where
-    (>|) :: a -> a -> Bool
-    (|<) :: a -> a -> Bool
-    (|<) = flip (>|)
-
-instance (Num a, Ord a) => Boundry (V.Vector a) where
-    x >| y = V.all (>0) $ fmap (\(a,b) -> a-b) (V.zip x y)
-
-transform :: (Boundry a, Boundry c) => Reversible a c -> Body a b -> Body c b
-transform g = Body . Prelude.map (\(f, mn ,mx) -> (f . backward g, forward g mn, forward g mx)) . unwrapList
-
-circle :: Body (V.Vector Double) Double
-circle = Body [(\v -> (V.sum . fmap (**2)) v - 1, V.fromList [-1, -1], V.fromList [1, 1])]
-
-moveX :: Num a => V.Vector a -> V.Vector a
-moveX v =  V.fromList [V.head v + 10] V.++ (V.tail v)
-
-movemX :: Num a => V.Vector a -> V.Vector a
-movemX v = V.fromList [V.head v - 10] V.++ (V.tail v)
-
-move :: Num a => Reversible (V.Vector a) (V.Vector a)
-move = Reversible moveX movemX
+--collision :: Body -> Body -> Bool
+collision b1 b2 =
+    let
+        p1 = pos b1
+        f [b1, b2] = b1^2 + b2^2
+    in
+        minimize NMSimplex2 1E-2 50 [1,1] f
