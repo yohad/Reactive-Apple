c33e1ade795977b27d025e385b1886467d956e1e
Yotam Ohad <yotam2206@gmail.com>
2017-11-21 14:35:43 +0000

    Added new libraries for function minimization

--- src/Apple.hs
+++ src/Apple.hs
@@ -5,17 +5,11 @@ import Data.Vector.Storable( (!)
                   , Vector
                   , fromList
                   , toList )
-import Numeric.GSL.Minimization
+import Numeric.AD
+import Numeric.Optimization.Algorithms.HagerZhang05
 
 data Body = Body { pos :: (Vector Double) -> Double }
 
 circle :: Double -> Double -> Body
 circle x y = Body (\v -> (v!0-x)^2 + (v!1-y)^2 - 1)
 
---collision :: Body -> Body -> Bool
-collision b1 b2 =
-    let
-        p1 = pos b1
-        f [b1, b2] = b1^2 + b2^2
-    in
-        minimize NMSimplex2 1E-2 50 [1,1] f
