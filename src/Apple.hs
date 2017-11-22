37e0a8b4b8bdd70a2c189cb1b318c8228aef8e5c
Yotam Ohad <yotam2206@gmail.com>
2017-11-22 12:18:22 +0000

    Tried to do collisions

--- src/Apple.hs
+++ src/Apple.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Apple where
 
 import Data.Vector.Storable( (!)
@@ -8,5 +8,14 @@ import Data.Vector.Storable( (!)
 import Numeric.AD
 import Numeric.Optimization.Algorithms.HagerZhang05
 
+newtype Image s i = Image { project :: s -> i }
 
-
+collision b1 b2 =
+  let
+    params = defaultParameters
+    f :: (Num a) => v a -> a
+    f = project b1 ^2 + project b2 ^2
+    func = VFunction f
+    g = VGradient $ grad f
+  in
+    optimize params 0 (fromList [0,0]) func
