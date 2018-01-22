ec54e2b6764c406803a22c984dc08caf31987d19
Yotam Ohad <yotam2206@gmail.com>
2018-01-22 17:25:54 +0000

    minimize function

--- src/Min.hs
+++ src/Min.hs
@@ -9,3 +9,16 @@ gradient f x0 eps =
     expand as bs = (as ++ [head bs + eps] ++ tail bs) : expand (as ++ [head bs]) (tail bs)
   in
     map (\x1 -> (f x1 - f x0) / eps) (expand [] x0)
+
+minimize :: ([Double] -> Double) -> [Double] -> Double -> Double -> [[Double]]
+minimize f x0 eps delta | f x0 **2 < eps = [x0]
+                        | otherwise = x0 : minimize f (vectorSub x0 $ vectorSMul delta $ gradient f x0 eps) eps delta
+
+vectorSub :: [Double] -> [Double] -> [Double]
+vectorSub [] [] = []
+vectorSub (x:xs) (y:ys) = (x - y) : vectorSub xs ys
+
+vectorSMul :: Double -> [Double] -> [Double]
+vectorSMul s = map (s*)
+
+test = minimize (\(x:y:_) -> x**2 + y**2) [1,1] 1e-8 0.5
